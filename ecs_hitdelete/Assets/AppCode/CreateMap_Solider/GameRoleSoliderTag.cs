9896af8e9fd08d1da2789f0489fc7817d8604988
learngame@126.com <learngame@126.com>
2020-09-11 07:17:52 +0800

    Destroy
    destroy go

--- ecs_hitdelete/Assets/AppCode/CreateMap_Solider/GameRoleSoliderTag.cs
+++ ecs_hitdelete/Assets/AppCode/CreateMap_Solider/GameRoleSoliderTag.cs
@@ -9,4 +9,12 @@ public class GameRoleSoliderTag : MonoBehaviour
     public int m_IndexX = 0;
     public int m_IndexZ = 0;
     public int m_TargetIndex = 0;
+
+    private int m_LifeTimeMin = 15;
+    private int m_LifeTimeMax = 25;
+
+    private void Start()
+    {
+        Destroy(gameObject, UnityEngine.Random.Range(m_LifeTimeMin, m_LifeTimeMax));
+    }
 }
