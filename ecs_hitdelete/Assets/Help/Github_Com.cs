ec88331de801655bee3b30c50e4454ebf0a76d2e
learngame@126.com <learngame@126.com>
2020-09-17 00:18:29 +0800

    test
    test

---
+++ ecs_hitdelete/Assets/Help/Github_Com.cs
@@ -0,0 +1,2 @@
+﻿//
+//https://github.com/Unity-Technologies/DOTS-training-samples (old 2020-9-17)
---
+++ ecs_hitdelete/Assets/Help/HelpMP4.cs
@@ -0,0 +1,7 @@
+﻿//Game Academy.school
+//ecs systembase 新功能
+//https://www.youtube.com/watch?v=UUCv2nDAKg4
+//key: dots animation unity
+
+//ecs trigger
+//https://www.youtube.com/watch?v=08AZLMqplmI
