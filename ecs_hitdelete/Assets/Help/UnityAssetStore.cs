cbb06e2f473f8610eea941a0aa7d8b1f78e310a2
learngame@126.com <learngame@126.com>
2020-09-17 08:20:21 +0800

    pkg
    pkg

---
+++ ecs_hitdelete/Assets/Help/UnityAssetStore.cs
@@ -0,0 +1,6 @@
+﻿//gpu instancer
+//https://assetstore.unity.com/packages/tools/utilities/gpu-instancer-117566
+//Unity3D 群体实例化GPU Instancer - Crowd Animations 0.9.5包
+
+//show demo
+//https://www.gurbu.com/demobuildscrowd
