addc3f9e0907d77e5beb8f734b8496a4e4e30487
learngame@126.com <learngame@126.com>
2020-09-22 01:17:44 +0800

    SignalEmitterSoliderFight
    SignalEmitterSoliderFight

---
+++ ecs_hitdelete/Assets/102_demo_fight_film/AppCode/SignalReceive_FightEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+/// <summary>
+/// 拖拽到Hierarchy > SignalReceive GameObject
+/// 也能接收到 Add Signal Emitter 的消息, SignalEmitterSoliderFight is null error
+/// </summary>
+public class SignalReceive_FightEvent : MonoBehaviour,INotificationReceiver
+{
+    public void OnNotify(Playable origin, INotification notification, object context)
+    {
+        SignalEmitterSoliderFight gm = notification as SignalEmitterSoliderFight;
+        if (gm != null)
+        {
+            Debug.Log(gm.speed);
+        }
+    }
+}
---
+++ ecs_hitdelete/Assets/102_demo_fight_film/AppCode/SoliderGoMarker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+/// <summary>
+/// customize signal emitter, Timeline right > add
+/// </summary>
+public class SignalEmitterSoliderFight: Marker,INotification
+{
+    public float speed;
+
+    public PropertyName id
+    {
+        get
+        {
+            return new PropertyName();
+        }
+    }
+
+    
+}
