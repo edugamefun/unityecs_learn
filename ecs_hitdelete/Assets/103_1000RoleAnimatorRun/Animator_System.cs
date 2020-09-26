cd324b177492035e4d2b14aa76b6269394b7d39c
learngame@126.com <learngame@126.com>
2020-09-26 17:43:22 +0800

    anim_run_offset
    勾选Parameter Set Cycle Offset=anim_run_offset

---
+++ ecs_hitdelete/Assets/103_1000RoleAnimatorRun/Animator_System.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Animator >state hero_run_01 > Parameter Set Cycle Offset=anim_run_offset
+ */
+using UnityEngine;
+
+public class Animator_System : MonoBehaviour
+{
+    public Animator Animator;
+    private float m_RunCycleOffseetMin = 0.0f;
+    private float m_RunCycleOffseetMax = 1.0f;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+
+            float a = UnityEngine.Random.Range(m_RunCycleOffseetMin, m_RunCycleOffseetMax);
+            Animator.SetFloat("anim_run_offset", a);
+
+            Animator.SetTrigger("trigger_1");
+        }
+
+ 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            float a = 0.0f;
+            Animator.SetFloat("anim_run_offset", a);
+
+            Animator.SetTrigger("trigger_1");
+
+        }
+    }
+}
