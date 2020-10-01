6374d9a6765d801edb414cd3837a822f6e50feb6
learngame@126.com <learngame@126.com>
2020-10-01 09:36:08 +0800

    add anim
    add anim

--- ecs_hitdelete/Assets/103_1000RoleAnimatorRun/Animator_System.cs
+++ ecs_hitdelete/Assets/103_1000RoleAnimatorRun/Animator_System.cs
@@ -5,29 +5,59 @@ using UnityEngine;
 
 public class Animator_System : MonoBehaviour
 {
-    public Animator Animator;
+    public Animator m_Animator;
     private float m_RunCycleOffseetMin = 0.0f;
     private float m_RunCycleOffseetMax = 1.0f;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if (m_Animator != null)
+        //    {
+        //        float a = UnityEngine.Random.Range(m_RunCycleOffseetMin, m_RunCycleOffseetMax);
 
-            float a = UnityEngine.Random.Range(m_RunCycleOffseetMin, m_RunCycleOffseetMax);
-            Animator.SetFloat("anim_run_offset", a);
+        //        m_Animator.Play("hero_run_01", 0, a);
+        //        Debug.Log(Time.time + "play run start:" + a);
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("m_Animator is null");
+        //    }
+        //}
 
-            Animator.SetTrigger("trigger_1");
-        }
+        //if (Input.GetKeyDown(KeyCode.F1))
+        //{
+        //    if (m_Animator != null)
+        //    {
 
- 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            float a = 0.0f;
-            Animator.SetFloat("anim_run_offset", a);
+        //        m_Animator.Play("hero_run_01");
+        //        Debug.Log(Time.time + "play run start:");
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("m_Animator is null");
+        //    }
+        //}
 
-            Animator.SetTrigger("trigger_1");
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
 
-        }
+        //    float a = UnityEngine.Random.Range(m_RunCycleOffseetMin, m_RunCycleOffseetMax);
+        //    //m_Animator.SetFloat("anim_run_offset", a);
+
+        //    m_Animator.Play("hero_run_01", 0, a);
+        //    //m_Animator.SetTrigger("trigger_1");
+        //}
+
+
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    float a = 0.0f;
+        //    m_Animator.SetFloat("anim_run_offset", a);
+
+        //    m_Animator.SetTrigger("trigger_1");
+
+        //}
     }
 }
---
+++ ecs_hitdelete/Assets/103_1000RoleAnimatorRun/Animator_System_Offset.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator_System_Offset : MonoBehaviour
+{
+    public Animator m_Animator;
+    private float m_RunCycleOffseetMin = 0.0f;
+    private float m_RunCycleOffseetMax = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            if (m_Animator != null)
+            {
+                float a = UnityEngine.Random.Range(m_RunCycleOffseetMin, m_RunCycleOffseetMax);
+                m_Animator.SetFloat("anim_run_offset", a);
+
+                m_Animator.SetTrigger("trigger_1");
+            }
+            else
+            {
+                Debug.LogError("m_Animator is null");
+            }
+        }
+    }
+}
+
+/*
+if (Input.GetKeyDown(KeyCode.F5))
+{
+    if (m_Animator != null)
+    {
+        float a = UnityEngine.Random.Range(m_RunCycleOffseetMin, m_RunCycleOffseetMax);
+
+        m_Animator.Play("hero_run_01", 0, a);
+        Debug.Log(Time.time + "play run start:" + a);
+    }
+    else
+    {
+        Debug.LogError("m_Animator is null");
+    }
+}
+ */
